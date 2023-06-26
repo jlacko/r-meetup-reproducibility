fbe2646219044cb5ccafa4224d6fa9e920a4c869
Jindra Lacko <jindra.lacko@gmail.com>
2023-06-26 14:37:57 +0200

    dockertuning

--- build_strnadi.sh
+++ build_strnadi.sh
@@ -1,4 +1,4 @@
 # vystavět z dockerfilu v aktuálním adresáři (je jediný) image s názvem strnadi
 # výstup stavět do logu (kdyby něco spadlo... :)
-docker build -t strnadi . | tee ../build.log
+docker build -t strnadi ./strnadi | tee ../build.log
 
---
+++ run_strnadi.sh
@@ -0,0 +1,14 @@
+# Spustit připravený kontejner strnadi
+# + přeskočit uživatelskou autentikaci (jsme na lokálu)
+# + připojit aktuální adresář jako home
+# + všechno se odehrává na portu 8787 (= default pro RStudio server version)
+docker run \
+ -e DISABLE_AUTH=true \
+ -v $(pwd)/:/home/rstudio/ \
+ --rm \
+ -p 8787:8787 \
+ strnadi
+
+# clean up detritus...
+rm -rf .config
+rm -rf .local
